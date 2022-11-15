37d787ebd30545a52011a4f19cf4926470dd5ca9
tegi <bapooltegi@gmail.com>
2022-11-15 23:34:26 +0900

    coinTracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>{/*{}안에 문자열을 쓰려면 백틱으로 쓰는거 잊지말기*/}
+      {loading ? <strong>Loading...</strong> :       <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>}
+
+      {/* coins배열에 있는 데이터를 map()으로 나열하는데 함수로 리스트를 뿌려줌.ㅇㅋ */}
     </div>
   );
 }
